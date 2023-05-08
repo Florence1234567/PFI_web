940f3acd571a4bd38a29aadea3ec50275f10ce2d
Florence1234567 <119359132+Florence1234567@users.noreply.github.com>
2023-05-08 09:29:09 -0400

    a

---
+++ ChatManager/Controllers/ChatController.cs
@@ -0,0 +1,18 @@
+﻿using ChatManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChatManager.Controllers
+{
+    public class ChatController : Controller
+    {
+        [OnlineUsers.UserAccess]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
