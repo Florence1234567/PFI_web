178bb7fbd6f52d372e9de9ad5496ad7accc01d2e
NoahGendron <94932280+NoahGendron@users.noreply.github.com>
2023-05-22 19:24:31 -0400

    chat

--- Models/ChatMessageRepository.cs
+++ Models/ChatMessageRepository.cs
@@ -7,5 +7,55 @@ namespace ChatManager.Models
 {
     public class ChatMessageRepository : Repository<ChatMessage>
     {
+        public ChatMessage Create(ChatMessage chatMessage)
+        {
+            try
+            {
+                chatMessage.Id = base.Add(chatMessage);
+                return chatMessage;
+            }
+            catch (Exception ex)
+            {
+
+                System.Diagnostics.Debug.WriteLine($"Add chatmessage failed : Message - {ex.Message}");
+            }
+            return null;
+        }
+
+        public override bool Update(ChatMessage chatMessage)
+        {
+            try
+            {
+                return base.Update(chatMessage);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine($"Update chatMessage failed : Message - {ex.Message}");
+            }
+            return false;
+        }
+
+        public override bool Delete(int chatMessageId)
+        {
+            try
+            {
+                ChatMessage chatMessageToDelete = DB.ChatMessages.Get(chatMessageId);
+                if (chatMessageToDelete != null)
+                {
+                    BeginTransaction();
+                    base.Delete(chatMessageId);
+                    EndTransaction();
+                    return true;
+                }
+
+                return false;
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine($"Remove chatmessage failed : Message - {ex.Message}");
+                EndTransaction();
+                return false;
+            }
+        }
     }
 }
