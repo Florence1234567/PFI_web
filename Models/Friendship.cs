7c4b58ad1d0cff8814ff2ca4a617f10ac43ac1b3
Florence1234567 <119359132+Florence1234567@users.noreply.github.com>
2023-05-22 15:24:19 -0400

    a

--- Models/Friendship.cs
+++ Models/Friendship.cs
@@ -7,5 +7,13 @@ namespace ChatManager.Models
 {
     public class Friendship
     {
+        public Friendship()
+        {
+            Status = 0;
+        }
+
+        public int IdUser1 { get; set; }
+        public int IdUser2 { get; set; }
+        public int Status { get; set; }
     }
 }
